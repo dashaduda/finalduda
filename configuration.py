1e43109b972ce161731ea6823f1c1e942047f830
dashaduda <157531620+dashaduda@users.noreply.github.com>
2024-02-16 14:52:56 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://7d401cb2-1bb2-4088-ac87-3a9e79e7cfa3.serverhub.praktikum-services.ru"
+CREATE_NEW_ORDER = "/api/v1/orders"
+RECEIVING_ORDER_BY_ID = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,11 @@
+order_body = {
+    "firstName": "Глеб",
+    "lastName": "Тарасов",
+    "address": "Никитская 5",
+    "metroStation": 3,
+    "phone": "+7 961 065 77 77",
+    "rentTime": 3,
+    "deliveryDate": "2024-02-16",
+    "comment": "Позвонить заранее",
+    "color": ["BLACK"]
+}
